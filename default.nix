56e99fbc376c8dfd64f281f397c85a43c0ef0c35
adisbladis <adisbladis@gmail.com>
2020-11-19 23:13:20 +0100

    Run nixpkgs-fmt on default.nix

--- default.nix
+++ default.nix
@@ -1,15 +1,12 @@
-{
-  pkgs ? import <nixpkgs> {}
-  , lib ? pkgs.lib
-  , name ? "nix"
-  , tag ? "latest"
-  , crossSystem ? null
-  , channelName ? "nixpkgs"
-  , channelURL ? "https://nixos.org/channels/nixpkgs-unstable"
+{ pkgs ? import <nixpkgs> { }
+, lib ? pkgs.lib
+, name ? "nix"
+, tag ? "latest"
+, crossSystem ? null
+, channelName ? "nixpkgs"
+, channelURL ? "https://nixos.org/channels/nixpkgs-unstable"
 }:
-
 let
-
   buildPkgs = if crossSystem != null && crossSystem != pkgs.system then pkgs.pkgsCross.${crossSystem} else pkgs;
 
   users = {
@@ -22,17 +19,19 @@ let
     };
 
   } // lib.listToAttrs (
-    map (
-      n: {
-        name = "nixbld${toString n}";
-        value = {
-          uid = 30000 + n;
-          gid = 30000;
-          groups = [ "nixbld" ];
-          description = "Nix build user ${toString n}";
-        };
-      }
-    ) (lib.lists.range 1 32)
+    map
+      (
+        n: {
+          name = "nixbld${toString n}";
+          value = {
+            uid = 30000 + n;
+            gid = 30000;
+            groups = [ "nixbld" ];
+            description = "Nix build user ${toString n}";
+          };
+        }
+      )
+      (lib.lists.range 1 32)
   );
 
   groups = {
@@ -47,53 +46,63 @@ let
     , home ? "/var/empty"
     , description ? ""
     , shell ? "/bin/false"
-    , groups ? []
+    , groups ? [ ]
     }: "${k}:x:${toString uid}:${toString gid}:${description}:${home}:${shell}"
   );
   passwdContents = (
     lib.concatStringsSep "\n"
-    (lib.attrValues (lib.mapAttrs userToPasswd users))
+      (lib.attrValues (lib.mapAttrs userToPasswd users))
   );
 
   userToShadow = k: { ... }: "${k}:!:1::::::";
   shadowContents = (
     lib.concatStringsSep "\n"
-    (lib.attrValues (lib.mapAttrs userToShadow users))
+      (lib.attrValues (lib.mapAttrs userToShadow users))
   );
 
   # Map groups to members
   # {
   #   group = [ "user1" "user2" ];
   # }
-  groupMemberMap = (let
-    # Create a flat list of user/group mappings
-    mappings = (
-      builtins.foldl' (
-        acc: user: let
-          groups = users.${user}.groups or [];
-        in acc ++ map (group: {
-          inherit user group;
-        }) groups
-      )
-      []
-      (lib.attrNames users)
-    );
-  in (
-    builtins.foldl' (
-      acc: v: acc // {
-        ${v.group} = acc.${v.group} or [] ++ [ v.user ];
-      }
-    )
-    {}
-    mappings)
+  groupMemberMap = (
+    let
+      # Create a flat list of user/group mappings
+      mappings = (
+        builtins.foldl'
+          (
+            acc: user:
+              let
+                groups = users.${user}.groups or [ ];
+              in
+              acc ++ map
+                (group: {
+                  inherit user group;
+                })
+                groups
+          )
+          [ ]
+          (lib.attrNames users)
+      );
+    in
+    (
+      builtins.foldl'
+        (
+          acc: v: acc // {
+            ${v.group} = acc.${v.group} or [ ] ++ [ v.user ];
+          }
+        )
+        { }
+        mappings)
   );
 
-  groupToGroup = k: { gid }: let
-    members = groupMemberMap.${k} or [];
-  in "${k}:x:${toString gid}:${lib.concatStringsSep "," members}";
+  groupToGroup = k: { gid }:
+    let
+      members = groupMemberMap.${k} or [ ];
+    in
+    "${k}:x:${toString gid}:${lib.concatStringsSep "," members}";
   groupContents = (
     lib.concatStringsSep "\n"
-    (lib.attrValues (lib.mapAttrs groupToGroup groups))
+      (lib.attrValues (lib.mapAttrs groupToGroup groups))
   );
 
   nixConf = {
@@ -103,52 +112,55 @@ let
   };
   nixConfContents = (lib.concatStringsSep "\n" (lib.mapAttrsFlatten (n: v: "${n} = ${v}") nixConf)) + "\n";
 
-  baseSystem = let
-    nixpkgs = buildPkgs.path;
-    channel = buildPkgs.runCommand "channel-nixos" {} ''
-      mkdir $out
-      ln -s ${nixpkgs} $out/nixpkgs
-      echo "[]" > $out/manifest.nix
+  baseSystem =
+    let
+      nixpkgs = buildPkgs.path;
+      channel = buildPkgs.runCommand "channel-nixos" { } ''
+        mkdir $out
+        ln -s ${nixpkgs} $out/nixpkgs
+        echo "[]" > $out/manifest.nix
+      '';
+    in
+    buildPkgs.runCommand "base-system"
+      {
+        inherit passwdContents groupContents shadowContents nixConfContents;
+        passAsFile = [
+          "passwdContents"
+          "groupContents"
+          "shadowContents"
+          "nixConfContents"
+        ];
+        allowSubstitutes = false;
+        preferLocalBuild = true;
+      } ''
+      env
+      set -x
+      mkdir -p $out/etc
+
+      cat $passwdContentsPath > $out/etc/passwd
+      echo "" >> $out/etc/passwd
+
+      cat $groupContentsPath > $out/etc/group
+      echo "" >> $out/etc/group
+
+      cat $shadowContentsPath > $out/etc/shadow
+      echo "" >> $out/etc/shadow
+
+      mkdir $out/tmp
+
+      mkdir -p $out/etc/nix
+      cat $nixConfContentsPath > $out/etc/nix/nix.conf
+
+
+      mkdir -p $out/nix/var/nix/profiles/per-user/root
+      ln -s ${channel} $out/nix/var/nix/profiles/per-user/root/channels-1-link
+      ln -s $out/nix/var/nix/profiles/per-user/root/channels-1-link $out/nix/var/nix/profiles/per-user/root/channels
+
+      mkdir -p $out/root
+      mkdir -p $out/root/.nix-defexpr
+      ln -s $out/nix/var/nix/profiles/per-user/root/channels $out/root/.nix-defexpr/channels
+      echo "${channelURL} ${channelName}" > $out/root/.nix-channels
     '';
-  in buildPkgs.runCommand "base-system" {
-    inherit passwdContents groupContents shadowContents nixConfContents;
-    passAsFile = [
-      "passwdContents"
-      "groupContents"
-      "shadowContents"
-      "nixConfContents"
-    ];
-    allowSubstitutes = false;
-    preferLocalBuild = true;
-  } ''
-    env
-    set -x
-    mkdir -p $out/etc
-
-    cat $passwdContentsPath > $out/etc/passwd
-    echo "" >> $out/etc/passwd
-
-    cat $groupContentsPath > $out/etc/group
-    echo "" >> $out/etc/group
-
-    cat $shadowContentsPath > $out/etc/shadow
-    echo "" >> $out/etc/shadow
-
-    mkdir $out/tmp
-
-    mkdir -p $out/etc/nix
-    cat $nixConfContentsPath > $out/etc/nix/nix.conf
-
-
-    mkdir -p $out/nix/var/nix/profiles/per-user/root
-    ln -s ${channel} $out/nix/var/nix/profiles/per-user/root/channels-1-link
-    ln -s $out/nix/var/nix/profiles/per-user/root/channels-1-link $out/nix/var/nix/profiles/per-user/root/channels
-
-    mkdir -p $out/root
-    mkdir -p $out/root/.nix-defexpr
-    ln -s $out/nix/var/nix/profiles/per-user/root/channels $out/root/.nix-defexpr/channels
-    echo "${channelURL} ${channelName}" > $out/root/.nix-channels
-  '';
 
 in
 buildPkgs.dockerTools.buildLayeredImageWithNixDb {
